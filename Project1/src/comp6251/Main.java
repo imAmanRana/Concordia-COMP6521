4f729136665af78e16719c0288f72439c659887d
Aman <amandeep_rana@ymail.com>
2018-02-13 21:30:44 -0500

    resources commit

---
+++ Project1/src/comp6251/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package comp6251;
+
+/**
+ * @author AmanRana
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
