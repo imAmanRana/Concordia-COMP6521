fbbe347e501a580014d5d81865f04ba9d1b5ab22
Aman <amandeep_rana@ymail.com>
2018-02-13 21:40:08 -0500

    added Constants

---
+++ Project1/src/comp6251/Constants.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package comp6251;
+
+/**
+ * @author AmanRana
+ *
+ */
+public class Constants {
+	
+	public static final int BLOCK_SIZE = 4096;
+	public static final int TUPLES_IN_BLOCK = 40;
+
+}
